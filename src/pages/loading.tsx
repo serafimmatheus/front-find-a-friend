6940ce61544620fd5cb13508c5512ae9d7c31f22
serafim <matheus18serafim@gmail.com>
2023-07-23 14:59:49 -0300

    feat: adicionando tela de carregamento

---
+++ src/pages/loading.tsx
@@ -0,0 +1,5 @@
+export default function Spinner() {
+  return (
+    <div className="w-12 h-12  rounded-full animate-spin border-x-8 border-solid border-orange-400 border-t-transparent"></div>
+  );
+}
